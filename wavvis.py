2b7daa640f7b9334a8cc67b3d28aaea37aa24dfa
Ryan-J-Bender <116182464+Ryan-J-Bender@users.noreply.github.com>
2022-12-05 01:09:59 -0500

    mostly there update. just needs to return values

--- wavvis.py
+++ wavvis.py
@@ -29,8 +29,8 @@ WIDTH = 1250
 C = 299792458
 
 # Display
-# display_surface = pg.display.set_mode((WIDTH, HEIGHT))
-# pg.display.set_caption("Python .wav Visualizer")
+display_surface = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Python .wav Visualizer")
 
 color = (51, 79, 124)
 
@@ -48,25 +48,26 @@ sound.export(newfilename, format="wav")
 # Reads values into data list
 data, rate = sf.read(newfilename)
 
-
-
+# Convert to int array ant resize array
 data = numpy.array(data)
 data = data.astype(int)
 
+
 for i in data:
-    
-    data[i] * 1000
-
-    if data[i] == 0:
-        data[i] = 1
-
-    if i % 9 != 0:
+    if i % 1470 != 0:
         data = numpy.delete(data, i)
 
 for i in data:
-    
-    if data[i] == 0:
+
+    data[i] = data[i] * 1000
+
+    if data[i] < 1:
         data[i] = 1
+    
+
+numpy.round(data, 0)
+
+
 
 # Prints info
 print ("The sample rate is " + str(rate) + " samples per second")
@@ -74,23 +75,6 @@ print ("The sample rate is " + str(rate) + " samples per second")
 print ("There are " + str(len(data)) + " samples.")
 
 print ("Then the audio length is: " + str(len(data) / rate) + " seconds.")
-
-
-# for j in data:
-#     data[j] = int(data[j])
-
-
-
-
-# Prints data and graph
-# # print(fixeddata)
-
-# # plt.plot(fixeddata)
-
-# # plt.show()
-
-# # print(fixeddata.max())
-# # print(fixeddata.min())
 
 # Plays audio
 pg.mixer.music.load(newfilename)
@@ -105,30 +89,23 @@ pg.display.update()
 # iterator
 i = 0
 
-print(data)
-
-# # # game loop
-# # while loop == True:
-# #     for event in pg.event.get():
-# #         if event.type == pg.QUIT:
-# #             loop = False
+# game loop
+while loop == True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            loop = False
     
-# #     red = (WaveToRGB.wav_to_rgb(data[i], 0))
-# #     green = (WaveToRGB.wav_to_rgb(data[i], 1))
-# #     blue = (WaveToRGB.wav_to_rgb(data[i], 2))
-
-# #     print(red)
-# #     print(green)
-# #     print(blue)
+    red = (WaveToRGB.wav_to_redgb(data[i]))
+    green = (WaveToRGB.wav_to_rgreenb(data[i]))
+    blue = (WaveToRGB.wav_to_rgblue(data[i]))
 
 
-# #     color = (red, green, blue)
-# #     display_surface.fill(color)
-# #     pg.display.update()
+    color = (red, green, blue)
+    display_surface.fill(color)
+    pg.display.update()
 
-# #     fpsClock.tick(FPS)
+    fpsClock.tick(FPS)
 
-# #     i += 1
+    i += 1
 
-# pg.quit()
-
+pg.quit()
